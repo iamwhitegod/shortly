b286be55d64140be5abf6e670e18d8b80c42a688
whitegod <kmbagwu12@gmail.com>
2021-08-14 10:14:18 +0100

    Fully responsive

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
+import sprite from "../../assets/sprite.svg";
 
 import Button from "../Button";
 
 function Nav() {
+  const toggleMenu = (e) => {
+    const parentEl = e.target
+      .closest(".nav")
+      .querySelector(".nav__menu-wrapper")
+      .classList.toggle("hidden");
+    // const menu = e.target.closest(".nav__menu");
+    // console.log(parentEl.querySelector(".nav__menu-wrapper"));
+    console.log(parentEl);
+  };
+
   return (
     <nav className="nav">
       <div className="nav__logo">
         <img src={logo} alt="brand logo" />
       </div>
 
-      <ul className="nav__list">
-        <li className="nav__item">
-          <Link to="#">Features</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="#">Pricing</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="#">Resourses</Link>
-        </li>
-      </ul>
+      <div className="nav__menu-wrapper">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <Link to="#">Features</Link>
+          </li>
+          <li className="nav__item">
+            <Link to="#">Pricing</Link>
+          </li>
+          <li className="nav__item">
+            <Link to="#">Resourses</Link>
+          </li>
+        </ul>
 
-      <div className="nav__btns">
-        <Button>Login</Button>
-        <Button classname="btn--active">Sign Up</Button>
+        <div className="nav__btns">
+          <Button>Login</Button>
+          <Button classname="btn--active">Sign Up</Button>
+        </div>
+      </div>
+
+      <div className="nav__menu hidden" onClick={toggleMenu}>
+        <svg className="nav__menu--icon">
+          <use href={sprite + "#menu"}></use>
+        </svg>
       </div>
     </nav>
   );
--- src/components/ShortenUrl/index.js
+++ src/components/ShortenUrl/index.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Button from "../Button";
 
 function ShortenUrl() {
+  const [input, setInput] = useState();
+  const inputRef = useRef();
+
+  const onInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+  };
+
   return (
     <section className="shorten-url">
-      <form action="" className="shorten-url__form">
+      <form action="" className="shorten-url__form" onSubmit={onFormSubmit}>
         <div className="shorten-url__form-group">
           <input
+            ref={inputRef}
             type="text"
             placeholder="Shorten a link here"
             name="shorten-url"
             id="shorten-url"
+            onChange={onInputChange}
+            value={input}
           />
+          <small className="shorten-url__message">Please add a link</small>
           <Button type="submit" classname="btn--radius">
             Shorten it!
           </Button>
         </div>
-        {/* <small className="shorten-url__message shorten-url__message--error">
-          Please add a link
-        </small> */}
       </form>
 
       <div className="shorten-url__result">
